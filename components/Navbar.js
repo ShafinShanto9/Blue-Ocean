3b54949af0d6a0c5781cd55fc721dcb535ff58d7
shafin_shant0 <shafinshanto9@gmail.com>
2022-03-27 12:24:05 +0600

    menu component done

---
+++ components/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Link from 'next/link'
+import { Menu, MenuButton, MenuList, MenuItem, IconButton, Flex, Box, Spacer } from '@chakra-ui/react'
+import {FcMenu, FcHome, FcAbout} from 'react-icons/fc'
+import { BsSearch } from 'react-icons/bs'
+import { FiKey } from 'react-icons/fi'
+
+const Navbar = () => {
+  return (
+      <Flex p="2" borederBottom="1px" borderColor="gray.300" boxShadow='base' >
+          <Box fontSize="3xl" color="blue.300" fontWeight="bold">
+              <Link href="/" paddingLeft="2" >Blue Ocean</Link>
+          </Box>
+          <Spacer />
+          <Box>
+              <Menu>
+                  <MenuButton as={IconButton} icon={<FcMenu />} variant="outlined" color="blue.300" />
+                  <MenuList>
+                      <Link href="/" passHref>
+                          <MenuItem icon={<FcHome/>}>Home</MenuItem>
+                      </Link>
+                      <Link href="/search" passHref>
+                          <MenuItem icon={<BsSearch/>}>Search</MenuItem>
+                      </Link>
+                      <Link href="/search?purpose=for-sale" passHref>
+                          <MenuItem icon={<FcAbout/>}>Buy Property</MenuItem>
+                      </Link>
+                      <Link href="/search?purpose=for-rent" passHref>
+                          <MenuItem icon={<FiKey/>}>Rent Property</MenuItem>
+                      </Link>
+                  </MenuList>
+              </Menu>
+          </Box>
+    </Flex>
+  )
+}
+
+export default Navbar
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import Router from "next/router"
+import NProgress from "nprogress"
+import Head from "next/head"
+import Layout from "../components/Layout"
+import {ChakraProvider} from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+
+      </Head>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
+  )
 }
 
 export default MyApp
